ac1a66522121a3e605872b710979141846934c07
Nabeel Ahmad Khan <nabeelahmadkh@gmail.com>
2018-01-21 07:38:57 -0600

    initial commit

---
+++ AddData.js
@@ -0,0 +1,14 @@
+// Code for entering the blogs in the Firebase Database.
+
+
+
+
+var config = {
+	apiKey: "AIzaSyC-d8dYJrCubsmFdYn2FKDcq1jhwA9PE3I",
+	authDomain: "blogproject-f3538.firebaseapp.com",
+	databaseURL: "https://blogproject-f3538.firebaseio.com",
+	storageBucket: "blogproject-f3538.appspot.com"
+};
+
+// Initializing Firebase App
+var defaultApp = firebase.initializeApp(config);
